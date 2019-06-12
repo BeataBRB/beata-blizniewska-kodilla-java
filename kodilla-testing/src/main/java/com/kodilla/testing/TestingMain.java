319b8a3e8e6487f87a18a6ea2cffd30ea52649d9
unknown <bblizniewska.1988@gmail.com>
2019-06-12 19:22:43 +0200

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,15 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator simpleCalculator = new Calculator();
+        Calculator calculator = new Calculator();
+        int resultOne = simpleCalculator.addAToB(5, 5);
+        int resultTwo = simpleCalculator.subtractAFromB(5,7);
+
+        System.out.println(resultOne);
+        System.out.println(resultTwo);
     }
 }
