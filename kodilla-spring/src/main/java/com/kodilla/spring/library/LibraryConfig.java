46d3a7036fecc058bf328bcddf8c26b15f9028d5
unknown <bblizniewska.1988@gmail.com>
2019-09-01 11:52:05 +0200

    modul_14.2.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/LibraryConfig.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.library;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class LibraryConfig {
+
+}
