70661c87e1c794f352a604ef7b1fe44f88cc4960
unknown <bblizniewska.1988@gmail.com>
2020-02-08 18:59:30 +0100

    zadanie 24.2

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/pizza/AbstractPizzaOrderDecorator.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import java.util.List;
+
+public class AbstractPizzaOrderDecorator implements PizzaOrder {
+    private final PizzaOrder pizzaOrder;
+
+    public AbstractPizzaOrderDecorator(PizzaOrder pizzaOrder) {
+        this.pizzaOrder = pizzaOrder;
+    }
+
+    @Override
+    public double getCost() {
+        return pizzaOrder.getCost();
+    }
+
+    @Override
+    public List<String> getIngredients() {
+        return pizzaOrder.getIngredients();
+    }
+}
