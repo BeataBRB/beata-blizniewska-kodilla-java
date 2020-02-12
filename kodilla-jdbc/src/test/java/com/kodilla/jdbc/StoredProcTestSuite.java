4d02a6a190b2d460b4c491efa380b64cc286a9d7
unknown <bblizniewska.1988@gmail.com>
2020-02-12 21:56:49 +0100

    zadanie 30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -18,11 +18,9 @@ public class StoredProcTestSuite {
         String sqlUpdate = "UPDATE READERS SET VIP_LEVEL =\"Not set\"";
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
-
         //When
         String sqlProcedureCall = "CALL UpdateVipLevels()";
         statement.execute(sqlProcedureCall);
-
         //Then
         String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM readers WHERE vip_level=\"Not set\"";
         ResultSet rs = statement.executeQuery(sqlCheckTable);
@@ -31,5 +29,25 @@ public class StoredProcTestSuite {
             howMany = rs.getInt("how_many");
         }
         assertEquals(0, howMany);
+    }
+    @Test
+    public void testUpdateBestsellers() throws SQLException{
+        //Given
+        DbManager dbManager=DbManager.getInstance();
+        String sqlUpdate="UPDATE BOOKS SET BESTSELLER=null";
+        Statement statement=dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall="CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        //Test
+        String sqlCheckTable="SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER IS NULL";
+        ResultSet rs=statement.executeQuery(sqlCheckTable);
+        int howMany=-1;
+        if(rs.next()){
+            howMany=rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(6,howMany);
+
     }
 }
